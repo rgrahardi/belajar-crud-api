2717a165d434d275b77ddf134fd425177d7fcbcb
rgrahardi <rgrahardi2019@gmail.com>
2021-03-19 18:06:15 +0700

    add function show by id and update api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,16 +23,41 @@ class ProductController extends Controller
     }
 
     function get(){
-         return response()->json(
+        $data = Product::all();
+        return response()->json(
         [
-            "message" => "Get Method Success"
+            "message" => "Success",
+            "data" => $data
         ]);
     }
 
-    function put($id){
-         return response()->json([
-        "message" => "Put Method Success" . $id
-    ]);
+    function getById($id){
+        $data = Product::where('id', $id)->get();
+        return response()->json(
+        [
+            "message" => "Success",
+            "data" => $data
+        ]);
+    }
+
+    function put($id, Request $request){
+        $product = Product::where('id', $id)->first();
+        if($product){
+            $product->name = $request->name ? $request->name : $product->name;
+            $product->price = $request->price ? $request->price : $product->price;
+            $product->quantity = $request->quantity ? $request->quantity : $product->quantity;
+            $product->active = $request->active ? $request->active : $product->active;
+            $product->description = $request->description ? $request->description : $product->description;
+            $product->save();
+            return response()->json([
+            "message" => "Put Method Success ",
+            "data" => $product
+        ]);
+        }
+            return response()->json([
+            "message" => "Product with id " . $id . " Not Found"
+            ], 400);
+       
     }
 
     function delete($id){
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Http\Request;
 
 Route::get('/products', 'ProductController@get');
 
+Route::get('/product/{id}', 'ProductController@getById');
+
 Route::post('/product', 'ProductController@post');
 
 Route::put('/product/{id}', 'ProductController@put');
